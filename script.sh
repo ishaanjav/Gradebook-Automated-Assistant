8e136069b2386c03a12daed41e21014144eb891c
Ishaan Javali <ishaanjav@gmail.com>
2021-09-17 20:15:21 -0500

    Demo and bash script

---
+++ script.sh
@@ -0,0 +1,6 @@
+# BASH script to run the python file from any directory.
+# Just copy and paste this function into your .bash_profile or .bashrc after specyifying the file's path
+
+gradebook(){
+	python3 "/<PATH_TO_PYTHON_FILE>/grades.py"
+}
